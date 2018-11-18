1eb676b0543e2cfaf4f015bef2cc769de01e2b14
Constanza Abarca <costi.abarca@gmail.com>
2018-11-18 15:11:36 -0400

    arreglando detalles

--- Asignaturas/admin.py
+++ Asignaturas/admin.py
@@ -1,11 +1,11 @@
-# from django.contrib import admin
-# from .models import *
+from django.contrib import admin
+from .models import *
 
-# # Modelos para ser vistos y modificados desde el Django admin
-# admin.site.register(Departamento)
-# admin.site.register(Programa)
-# admin.site.register(Asignatura)
-# #admin.site.register(User)
-# admin.site.register(Componente)
-# admin.site.register(Rol)
-# admin.site.register(Area)
+# Modelos para ser vistos y modificados desde el Django admin
+admin.site.register(Departamento)
+admin.site.register(Programa)
+admin.site.register(Asignatura)
+#admin.site.register(User)
+admin.site.register(Componente)
+admin.site.register(Rol)
+admin.site.register(Area)
