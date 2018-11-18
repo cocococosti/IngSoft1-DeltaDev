17a234c9a880e4243acbced177d700a1f61478a0
Constanza Abarca <costi.abarca@gmail.com>
2018-11-18 15:47:29 -0400

    arreglando campos de la base

--- Asignaturas/models.py
+++ Asignaturas/models.py
@@ -4,12 +4,11 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator
-from django.contrib.postgres.fields import ArrayField
 
 # Modelos de cada Tabla y sus atributos
 
 class Departamento(models.Model):
-    codigo = models.CharField(primary_key=True, max_length=15)
+    codigo = models.CharField(primary_key=True, max_length=3)
     nombre = models.CharField(max_length=60, unique=True)
 
 class Asignatura(models.Model):
@@ -19,5 +18,5 @@ class Asignatura(models.Model):
     horasTeoria = models.IntegerField()
     horasPractica = models.IntegerField()
     horasLab = models.IntegerField()
-    requisitos =  ArrayField(models.CharField(max_length=200))
+    requisitos = models.ManyToManyField(self, blank=True)
     departamento = models.ForeignKey(Departamento, default="",on_delete=models.CASCADE)
